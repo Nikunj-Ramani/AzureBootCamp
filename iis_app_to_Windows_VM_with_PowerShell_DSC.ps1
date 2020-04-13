f231c9055cd0ecc5bac351158169cfaf585a74a7
Nikunj-Ramani <63297272+Nikunj-Ramani@users.noreply.github.com>
2020-04-13 10:30:37 +0530

    Create iis_app_to_Windows_VM_with_PowerShell_DSC.ps1
    this file is for Install an iis app to a Windows VM with PowerShell DSC

---
+++ iis_app_to_Windows_VM_with_PowerShell_DSC.ps1
@@ -0,0 +1,29 @@
+# Define the script for your Desired Configuration to download and run
+$dscConfig = @{
+  "wmfVersion" = "latest";
+  "configuration" = @{
+    "url" = "https://github.com/Azure-Samples/compute-automation-configurations/raw/master/dsc.zip";
+    "script" = "configure-http.ps1";
+    "function" = "WebsiteTest";
+  };
+}
+
+# Get information about the scale set
+$vmss = Get-AzVmss `
+                -ResourceGroupName "myResourceGroup" `
+                -VMScaleSetName "myScaleSet"
+
+# Add the Desired State Configuration extension to install IIS and configure basic website
+$vmss = Add-AzVmssExtension `
+    -VirtualMachineScaleSet $vmss `
+    -Publisher Microsoft.Powershell `
+    -Type DSC `
+    -TypeHandlerVersion 2.24 `
+    -Name "DSC" `
+    -Setting $dscConfig
+
+# Update the scale set and apply the Desired State Configuration extension to the VM instances
+Update-AzVmss `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myScaleSet"  `
+    -VirtualMachineScaleSet $vmss
